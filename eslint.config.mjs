9363828f79bb53c7873b8bb5d9dcf39581018bb3
Zarael25 <telecom25a@gmail.com>
2025-08-05 11:19:25 -0400

    Estructura: Frontend, archivos y directorios base

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import prettier from 'eslint-config-prettier/flat'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    ignores: ['node_modules/**'],
+  },
+  prettier,
+  {
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      'no-mixed-spaces-and-tabs': 'error',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+    },
+  },
+]
 
-export default eslintConfig;
+export default eslintConfig
