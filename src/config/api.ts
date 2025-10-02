73640001ddc0c765274b1da8ae7d156ef09afdd5
Zarael25 <telecom25a@gmail.com>
2025-10-02 11:25:00 -0400

    Se cambio la ruta a localhost

--- src/config/api.ts
+++ src/config/api.ts
@@ -61,7 +61,7 @@
 
 
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.101:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000";
 
 export const ENDPOINTS = {
   AUTH: {
