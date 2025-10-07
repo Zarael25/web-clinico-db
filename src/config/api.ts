5a2ceaf8b210124792c5ec8af505fb3b31dfbcec
Zarael25 <telecom25a@gmail.com>
2025-10-07 00:36:59 -0400

    Despliegue: Se actuializaron datos para despliegue 7

--- src/config/api.ts
+++ src/config/api.ts
@@ -61,8 +61,7 @@
 
 
 const API_BASE_URL =
-  //process.env.NEXT_PUBLIC_API_BASE_URL || "https://api-clinico-db.vercel.app";
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://192.168.1.101:3000";
+  process.env.NEXT_PUBLIC_API_BASE_URL || "https://api-clinico-db.vercel.app";
 
 export const ENDPOINTS = {
   AUTH: {
