e1a28645bede2c6a66817606166b3e18ed9c92ac
Zarael25 <telecom25a@gmail.com>
2025-10-11 14:06:05 -0400

    Rutas: Redireccion de la primera pagina 2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,14 @@
+import { cookies } from 'next/headers'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  redirect('/auth/login')
+  const token = cookies().get('token')?.value
+
+  if (token) {
+    // Si tiene token, lo mandamos al panel o dashboard
+    redirect('/estudiantes')
+  } else {
+    // Si no tiene token, al login
+    redirect('/auth/login')
+  }
 }
